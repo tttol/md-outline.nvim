05edf11fab110007d29e533b4a4a1ab48c63c35c
tttol <tkhstol.929@gmail.com>
2025-11-04 07:04:54 +0900

    Add auto-open feature for markdown files with configuration option

--- lua/md-outline.lua
+++ lua/md-outline.lua
@@ -4,12 +4,20 @@ local window = require('core.window')
 
 local outline_win = nil
 
+M.config = {
+  auto_open = true
+}
+
 function M.show()
     outline_win = window.show()
 end
 
 function M.close()
     outline_win = window.close(outline_win)
+end
+
+function M.setup(opts)
+  M.config = vim.tbl_extend('force', M.config, opts or {})
 end
 
 function M.main()
--- plugin/commands.lua
+++ plugin/commands.lua
@@ -10,3 +10,15 @@ end, {})
 vim.api.nvim_create_user_command('MdoClose', function()
   require('md-outline').close()
 end, {})
+
+local augroup = vim.api.nvim_create_augroup('MdOutline', { clear = true })
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = augroup,
+  pattern = '*.md',
+  callback = function()
+    local md_outline = require('md-outline')
+    if md_outline.config.auto_open then
+      md_outline.show()
+    end
+  end,
+})
